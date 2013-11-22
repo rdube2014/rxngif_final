e77ccdbf0e42a55c29d218cc4d3cbb377bf795c4
Raghu Betina <rbetina@gmail.com>
2013-11-22 09:40:08 -0600

    Before I rake db:migrate, I double-check my generated migration files to make sure they look good. In this case, I decide to add default values to a few columns so they don't start off as nil, and I request automatically-managed timestamp columns (created_at and updated_at) from the database with t.timestamps.

--- db/migrate/20131122153206_create_pictures.rb
+++ db/migrate/20131122153206_create_pictures.rb
@@ -1,9 +1,11 @@
 class CreatePictures < ActiveRecord::Migration
   def change
     create_table :pictures do |t|
-      t.string :image_url
+      t.string :image_url, :default => "http://upload.wikimedia.org/wikipedia/commons/9/92/Image_of_nothing.svg"
       t.string :caption
       t.integer :user_id
+
+      t.timestamps
     end
   end
 end
--- db/migrate/20131122153231_create_users.rb
+++ db/migrate/20131122153231_create_users.rb
@@ -1,8 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, :default => "Anonymous"
+      t.string :last_name, :default => "User"
+
+      t.timestamps
     end
   end
 end
