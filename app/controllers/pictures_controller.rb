093846ea0c1c41f26a3d42beb4c73c4bae60fd9e
Raghu Betina <rbetina@gmail.com>
2013-11-22 10:42:32 -0600

    Using a before_action filter to prevent users from modifying other users' pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,15 @@
 class PicturesController < ApplicationController
   before_action :set_picture, :only => [:show, :edit, :update, :destroy]
+  before_action :user_must_be_owner_of_picture, :only => [:edit, :udpate, :destroy]
 
   def set_picture
     @picture = User.find(params[:id])
+  end
+
+  def user_must_be_owner_of_picture
+    unless @picture.user == current_user
+      redirect_to root_url, :alert => "You are not authorized for that."
+    end
   end
 
   def index
